a0c618b4b0be440c3e3dff604a55336c1e948df2
RiskyKen <RiskyKen@users.noreply.github.com>
2019-03-02 19:13:03 +0000

    Fixed player inventory being rendered when the config option is turned off.

--- src/main/java/moe/plushie/rpgeconomy/currency/client/gui/GuiWallet.java
+++ src/main/java/moe/plushie/rpgeconomy/currency/client/gui/GuiWallet.java
@@ -12,10 +12,12 @@ import moe.plushie.rpgeconomy.core.common.inventory.slot.SlotCurrency;
 import moe.plushie.rpgeconomy.core.common.network.PacketHandler;
 import moe.plushie.rpgeconomy.core.common.network.client.MessageClientGuiButton;
 import moe.plushie.rpgeconomy.currency.common.Currency;
+import moe.plushie.rpgeconomy.currency.common.Currency.CurrencyVariant;
 import moe.plushie.rpgeconomy.currency.common.capability.CurrencyCapability;
 import moe.plushie.rpgeconomy.currency.common.inventory.ContainerWallet;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
@@ -60,19 +62,21 @@ public class GuiWallet extends GuiContainer {
         int startX = halfSizeX - halfSlotTotalWidth - 1;
         
         for (int i = 0; i < slotCount; i++) {
+            CurrencyVariant variant = currency.getCurrencyVariants()[i];
             buttonList.add(new GuiIconButton(this, i, getGuiLeft() + startX + (slotSize + slotSpacing) * i, getGuiTop() + 33, 18, 18, TEXTURE)
                     .setIconLocation(0, 220, 18, 18)
-                    .setHoverText("in")
+                    .setHoverText("+" + String.valueOf(variant.getValue()))
                     .setDrawButtonBackground(false));
             buttonList.add(new GuiIconButton(this, i + slotCount, getGuiLeft() + startX + (slotSize + slotSpacing) * i, getGuiTop() + 73, 18, 18, TEXTURE)
                     .setIconLocation(0, 238, 18, 18)
-                    .setHoverText("out")
+                    .setHoverText("-" + String.valueOf(variant.getValue()))
                     .setDrawButtonBackground(false));
         }
         
         buttonList.add(new GuiIconButton(this, -1, getGuiLeft() + startX - slotSize - slotSpacing * 2, getGuiTop() + 33, 18, 18, TEXTURE)
                 .setIconLocation(0, 202, 18, 18)
-                .setHoverText("all"));
+                .setHoverText("+*")
+                .setDrawButtonBackground(false));
     }
 
     @Override
@@ -91,14 +95,15 @@ public class GuiWallet extends GuiContainer {
     @Override
     protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
         mc.renderEngine.bindTexture(TEXTURE);
-        drawTexturedModalRect(getGuiLeft(), getGuiTop(), 0, 0, 176, 97);
+        drawTexturedModalRect(getGuiLeft(), getGuiTop(), 0, 0, TEXTURE_SIZE_X, TEXTURE_SIZE_Y);
         for (Slot slot : inventorySlots.inventorySlots) {
             if (slot instanceof SlotCurrency) {
                 drawTexturedModalRect(getGuiLeft() + slot.xPos - 1, getGuiTop() + slot.yPos - 1, 238, 0, 18, 18);
             }
-
         }
-        GuiHelper.renderPlayerInvTexture(getGuiLeft(), getGuiTop() + 98);
+        if (ConfigHandler.showPlayerInventoryInWalletGUI) {
+            GuiHelper.renderPlayerInvTexture(getGuiLeft(), getGuiTop() + TEXTURE_SIZE_Y + 1);
+        }
     }
 
     @Override
@@ -107,10 +112,20 @@ public class GuiWallet extends GuiContainer {
         int titleWidth = fontRenderer.getStringWidth(title);
         fontRenderer.drawString(title, xSize / 2 - titleWidth / 2, 6, 0x333333);
 
-        GuiHelper.renderPlayerInvlabel(0, 98, fontRenderer);
         String value = String.format(currency.getDisplayFormat(), wallet.getAmount());
         int valueWidth = fontRenderer.getStringWidth(value);
-
         fontRenderer.drawString(value, 139 - valueWidth, 21, 0x333333);
+        
+        if (ConfigHandler.showPlayerInventoryInWalletGUI) {
+            GuiHelper.renderPlayerInvlabel(0, TEXTURE_SIZE_Y + 1, fontRenderer);
+        }
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(-guiLeft, -guiTop, 0);
+        for (GuiButton button : buttonList) {
+            if (button instanceof GuiIconButton) {
+                //((GuiIconButton)button).drawRollover(mc, mouseX, mouseY);
+            }
+        }
+        GlStateManager.popMatrix();
     }
 }
