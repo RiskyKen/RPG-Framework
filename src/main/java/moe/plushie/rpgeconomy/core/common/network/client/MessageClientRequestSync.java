de35fa265f2d17d229c5495dbeba1f0cd1cfdf14
RiskyKen <RiskyKen@users.noreply.github.com>
2019-07-01 10:15:41 +0100

    Fixed dedicated server crash when trying to sync shop list to a client.

--- src/main/java/moe/plushie/rpgeconomy/core/common/network/client/MessageClientRequestSync.java
+++ src/main/java/moe/plushie/rpgeconomy/core/common/network/client/MessageClientRequestSync.java
@@ -2,8 +2,9 @@ package moe.plushie.rpgeconomy.core.common.network.client;
 
 import io.netty.buffer.ByteBuf;
 import moe.plushie.rpgeconomy.core.RpgEconomy;
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
@@ -31,39 +32,32 @@ public class MessageClientRequestSync implements IMessage, IMessageHandler<Messa
 
     @Override
     public IMessage onMessage(MessageClientRequestSync message, MessageContext ctx) {
-        Minecraft.getMinecraft().addScheduledTask(new SyncRequest(ctx.getServerHandler().player, message.syncType));
+        MinecraftServer server = FMLCommonHandler.instance().getMinecraftServerInstance();
+        EntityPlayerMP player = ctx.getServerHandler().player;
+        server.addScheduledTask(new Runnable() {
+            @Override
+            public void run() {
+                switch (message.syncType) {
+                case CURRENCIES:
+                    RpgEconomy.getProxy().getCurrencyManager().syncToClient(player);
+                    break;
+                case SHOPS_IDENTIFIERS:
+                    RpgEconomy.getProxy().getShopManager().syncToClient(player);
+                    break;
+                case MAIL_SYSTEMS:
+                    RpgEconomy.getProxy().getMailSystemManager().syncToClient(player);
+                    break;
+                }
+            }
+        });
         return null;
     }
     
+
+
     public static enum SyncType {
         CURRENCIES,
         SHOPS_IDENTIFIERS,
         MAIL_SYSTEMS
-    }
-    
-    public static class SyncRequest implements Runnable {
-        
-        private final EntityPlayerMP player;
-        private final SyncType syncType;
-        
-        public SyncRequest(EntityPlayerMP player, SyncType syncType) {
-            this.player = player;
-            this.syncType = syncType;
-        }
-
-        @Override
-        public void run() {
-            switch (syncType) {
-            case CURRENCIES:
-                RpgEconomy.getProxy().getCurrencyManager().syncToClient(player);
-                break;
-            case SHOPS_IDENTIFIERS:
-                RpgEconomy.getProxy().getShopManager().syncToClient(player);
-                break;
-            case MAIL_SYSTEMS:
-                RpgEconomy.getProxy().getMailSystemManager().syncToClient(player);
-                break;
-            }
-        }
     }
 }
