01b2cfbca6eda22fd603584fa8e28959823febc5
RiskyKen <RiskyKen@users.noreply.github.com>
2019-07-01 09:32:04 +0100

    Mod keys not being run on the main thread.

--- src/main/java/moe/plushie/rpgeconomy/core/common/network/client/MessageClientKeyPress.java
+++ src/main/java/moe/plushie/rpgeconomy/core/common/network/client/MessageClientKeyPress.java
@@ -3,6 +3,8 @@ package moe.plushie.rpgeconomy.core.common.network.client;
 import io.netty.buffer.ByteBuf;
 import moe.plushie.rpgeconomy.core.RpgEconomy;
 import moe.plushie.rpgeconomy.core.common.lib.LibModKeys.ModKey;
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
@@ -10,10 +12,10 @@ import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 public class MessageClientKeyPress implements IMessage, IMessageHandler<MessageClientKeyPress, IMessage> {
 
     private ModKey modKey;
-    
+
     public MessageClientKeyPress() {
     }
-    
+
     public MessageClientKeyPress(ModKey modKey) {
         this.modKey = modKey;
     }
@@ -22,15 +24,21 @@ public class MessageClientKeyPress implements IMessage, IMessageHandler<MessageC
     public void toBytes(ByteBuf buf) {
         buf.writeInt(modKey.ordinal());
     }
-    
+
     @Override
     public void fromBytes(ByteBuf buf) {
         modKey = ModKey.values()[buf.readInt()];
     }
-    
+
     @Override
     public IMessage onMessage(MessageClientKeyPress message, MessageContext ctx) {
-        RpgEconomy.getProxy().onClentKeyPress(ctx.getServerHandler().player, message.modKey);
+        MinecraftServer server = FMLCommonHandler.instance().getMinecraftServerInstance();
+        server.addScheduledTask(new Runnable() {
+            @Override
+            public void run() {
+                RpgEconomy.getProxy().onClentKeyPress(ctx.getServerHandler().player, message.modKey);
+            }
+        });
         return null;
     }
 }
