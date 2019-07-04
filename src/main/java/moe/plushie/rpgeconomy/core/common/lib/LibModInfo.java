f3cd56aec99eb175370ab3df7826cd673e8a8089
RiskyKen <RiskyKen@users.noreply.github.com>
2019-07-04 18:53:16 +0100

    Send version when checking for updates.

--- src/main/java/moe/plushie/rpgeconomy/core/common/lib/LibModInfo.java
+++ src/main/java/moe/plushie/rpgeconomy/core/common/lib/LibModInfo.java
@@ -6,7 +6,7 @@ public class LibModInfo {
     public static final String NAME = "RPG Economy";
     public static final String VERSION = "@VERSION@";
     public static final String CHANNEL = "rpgecon";
-    public static final String UPDATE_JSON_URL = "http://plushie.moe/app_update/minecraft_mods/rpg_economy/update.json";
+    public static final String UPDATE_JSON_URL = "http://plushie.moe/app_update/minecraft_mods/rpg_economy/update.json?version=" + VERSION;
 
     public static final String PROXY_CLIENT_CLASS = "moe.plushie.rpgeconomy.core.proxies.ClientProxy";
     public static final String PROXY_COMMNON_CLASS = "moe.plushie.rpgeconomy.core.proxies.CommonProxy";
