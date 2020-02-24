a4571a7ddeb46d9e0e63b3fc8ea72bc21286477b
RiskyKen <RiskyKen@users.noreply.github.com>
2020-02-24 14:11:28 +0000

    Better error handling when loading item matcher.

--- src/main/java/moe/plushie/rpg_framework/core/common/serialize/ItemMacherSerializer.java
+++ src/main/java/moe/plushie/rpg_framework/core/common/serialize/ItemMacherSerializer.java
@@ -36,18 +36,25 @@ public final class ItemMacherSerializer {
     }
 
     public static IItemMatcher deserializeJson(JsonElement jsonElement) {
-        return deserializeJson(jsonElement.getAsJsonObject());
-    }
-
-    public static IItemMatcher deserializeJson(JsonObject jsonObject) {
         try {
+            JsonObject jsonObject = jsonElement.getAsJsonObject();
             IItemMatcher matcher = null;
 
             if (jsonObject.has(PROP_TYPE_STACK)) {
-                JsonObject jsonStack = jsonObject.get(PROP_TYPE_STACK).getAsJsonObject();
-                ItemStack itemStack = SerializeHelper.readItemFromJson(jsonStack.get(PROP_ITEM));
-                boolean matchMeta = jsonStack.get(PROP_MATCH_META).getAsBoolean();
-                boolean matchNBT = jsonStack.get(PROP_MATCH_NBT).getAsBoolean();
+                ItemStack itemStack = ItemStack.EMPTY;
+                boolean matchMeta = false;
+                boolean matchNBT = false;
+
+                if (jsonObject.has(PROP_TYPE_STACK)) {
+                    JsonObject jsonStack = jsonObject.get(PROP_TYPE_STACK).getAsJsonObject();
+                    itemStack = SerializeHelper.readItemFromJson(jsonStack.get(PROP_ITEM));
+                    if (jsonStack.has(PROP_MATCH_META)) {
+                        matchMeta = jsonStack.get(PROP_MATCH_META).getAsBoolean();
+                    }
+                    if (jsonStack.has(PROP_MATCH_NBT)) {
+                        matchNBT = jsonStack.get(PROP_MATCH_NBT).getAsBoolean();
+                    }
+                }
                 matcher = new ItemMatcherStack(itemStack, matchMeta, matchNBT);
             }
             return matcher;
