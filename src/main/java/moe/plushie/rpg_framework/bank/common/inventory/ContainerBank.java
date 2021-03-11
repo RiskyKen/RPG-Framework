6e72287082864205820438311bbbd3584246dc7c
RiskyKen <RiskyKen@users.noreply.github.com>
2021-03-11 14:35:03 +0000

    Revert "Comment!"
    This reverts commit e666d48ceebaf03dbd4a0bc0060347e87135b72f.

--- src/main/java/moe/plushie/rpg_framework/bank/common/inventory/ContainerBank.java
+++ src/main/java/moe/plushie/rpg_framework/bank/common/inventory/ContainerBank.java
@@ -212,6 +212,7 @@ public class ContainerBank extends ModContainer implements IInventoryChangedList
         if (slot.getHasStack()) {
             ItemStack stack = slot.getStack();
             ItemStack result = stack.copy();
+            // Moving from tile entity to player.
             if (!this.mergeItemStack(stack, 0, getPlayerInvStartIndex(), false)) {
                 return ItemStack.EMPTY;
             }
